cafae5cdd556ca6c4df306077125a462ce2768f6
Mohan Karthik <nemesis.nitt@gmail.com>
2022-09-25 14:22:05 +0530

    Modified configs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? '/world-of-upbringing.github.io/' : '',
   reactStrictMode: true,
   swcMinify: true,
   images: {
