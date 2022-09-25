82b7a90f0f2420c9e02a50e283ea38717b9a21cf
Mohan Karthik <nemesis.nitt@gmail.com>
2022-09-25 17:40:33 +0530

    Reverted config for netlify

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? "/public-site/" : "",
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
+  swcMinify: true
 };
 
 module.exports = nextConfig;
