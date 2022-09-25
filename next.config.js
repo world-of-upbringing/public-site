a909a6eb2cbcb3a7cf22752db74d3ca354569252
Mohan Karthik <nemesis.nitt@gmail.com>
2022-09-25 14:23:19 +0530

    Fixed missing variable

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix: isProd ? '/world-of-upbringing.github.io/' : '',
