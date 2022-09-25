534cd87163c8421c88cf2b75729bc8593b624e09
Mohan Karthik <nemesis.nitt@gmail.com>
2022-09-25 14:29:51 +0530

    Updated site prefix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? '/world-of-upbringing.github.io/' : '',
+  assetPrefix: isProd ? '/public-site/' : '',
   reactStrictMode: true,
   swcMinify: true,
   images: {
