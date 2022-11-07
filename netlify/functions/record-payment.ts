9432f1d53502b312aad4ffe1ad4ab0017b66e8a0
Mohan Karthik <nemesis.nitt@gmail.com>
2022-11-07 23:49:25 +0530

    Added support to save payment in Google sheets

--- netlify/functions/record-payment.ts
+++ netlify/functions/record-payment.ts
@@ -1,7 +1,7 @@
 import { Handler } from "@netlify/functions";
 import { PrismaClient } from "@prisma/client";
 import * as crypto from "crypto";
-import querystring from "querystring";
+import { google } from "googleapis";
 
 const prisma = new PrismaClient();
 
@@ -27,16 +27,16 @@ const handler: Handler = async (event, context) => {
   }
   const params = new URLSearchParams(event.body ?? "");
 
-  console.log(params);
+  console.log(`Received request with ${params}`);
 
   if (event.body) {
     // Check if the mac is valid
-    // if (!_isTransactionValid(newTransaction)) {
-    //   return {
-    //     statusCode: 400,
-    //     body: "Invalid transaction"
-    //   };
-    // }
+    if (!_isTransactionValid(params)) {
+      return {
+        statusCode: 400,
+        body: "Invalid transaction",
+      };
+    }
 
     // Save the data to DB
     await prisma.transaction.create({
@@ -58,6 +58,9 @@ const handler: Handler = async (event, context) => {
       },
     });
 
+    // Update the date to Google sheets
+    await _updateSheet(params);
+
     // Return success
     return {
       statusCode: 200,
@@ -71,23 +74,45 @@ const handler: Handler = async (event, context) => {
   };
 };
 
+// Function to update the Google sheets with the payment details
+const _updateSheet = async (params: URLSearchParams): Promise<void> => {
+  const auth = new google.auth.JWT({
+    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+  const sheet = google.sheets("v4");
+  console.log(process.env.GOOGLE_SHEET_ID);
+  await sheet.spreadsheets.values.append({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    auth: auth,
+    range: "Sheet1",
+    valueInputOption: "RAW",
+    requestBody: {
+      values: [
+        [
+          new Date().toDateString(),
+          params.get("payment_id"),
+          params.get("status"),
+          params.get("currency"),
+          params.get("amount"),
+        ],
+      ],
+    },
+  });
+};
+
 // Checks the mac of the transaction using the algorithm described in
 // https://support.instamojo.com/hc/en-us/articles/207816249-What-is-the-Message-Authentication-Code-in-Webhook-
-const _isTransactionValid = (body: TransactionEntry): boolean => {
+const _isTransactionValid = (params: URLSearchParams): boolean => {
   const secret = process.env.INSTAMOJO_SECRET;
   if (secret) {
-    const mac = body.mac;
-    const newBody: { [id: string]: string | number } = {
-      ...body,
-    };
-    delete newBody["mac"];
+    const mac = params.get("mac");
+    params.sort();
     let concat = "";
-
-    Object.keys(newBody)
-      .sort()
-      .forEach((_, key) => (concat += String(newBody[key]))) + "|";
-
-    // Remove the last pipe
+    params.forEach((val, key) => {
+      if (key != "mac") concat += val + "|";
+    });
     concat = concat.slice(0, -1);
 
     const actual = crypto
