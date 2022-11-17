b9dc7ff73c05c208367f28040d32584ee03c955a
Mohan Karthik <nemesis.nitt@gmail.com>
2022-11-17 19:20:22 +0530

    Footer added

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,7 +1,81 @@
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+  faWhatsapp,
+  faInstagram,
+  faFacebook,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import IconText from "../common/icon_text";
+import { H4 } from "../common/wouH";
+import Link from "next/link";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 export default function Footer() {
   return (
-    <div id="footer" className="flex bg-white text-grey h-40 border-t-4">
-      <span className="mx-auto">The footer goes here</span>
+    <div id="footer" className="flex bg-light-grey text-white">
+      <div className="mx-auto">
+        <div className="text-center">
+          <H4>Contact Us</H4>
+        </div>
+
+        <div className="flex flex-row place-content-center">
+          <IconText
+            iconDef={faPhone}
+            text={"(+91) 797 595 8943"}
+            url="tel:+917975958943"
+          />
+        </div>
+
+        <div className="flex flex-row place-content-center">
+          <IconText
+            iconDef={faEnvelope}
+            text={"ila@worldofupbringing.com"}
+            url="mailto:ila@worldofupbringing.com"
+          />
+        </div>
+
+        <div className="flex flex-row my-3 place-content-center">
+          <Link href="https://api.whatsapp.com/send?phone=9986905689">
+            <a>
+              <FontAwesomeIcon
+                className="px-2 mx-auto"
+                size="2x"
+                icon={faWhatsapp as IconProp}
+              />
+            </a>
+          </Link>
+
+          <Link href="https://www.instagram.com/worldupbringing/">
+            <a>
+              <FontAwesomeIcon
+                className="px-2 mx-auto"
+                size="2x"
+                icon={faInstagram as IconProp}
+              />
+            </a>
+          </Link>
+
+          <Link href="https://www.facebook.com/worldofupbringing/">
+            <a>
+              <FontAwesomeIcon
+                className="px-2 mx-auto"
+                size="2x"
+                icon={faFacebook as IconProp}
+              />
+            </a>
+          </Link>
+
+          <Link href="https://www.youtube.com/channel/UCsv4oHXmwMCTEVTrZEanaHQ">
+            <a>
+              <FontAwesomeIcon
+                className="px-2 mx-auto"
+                size="2x"
+                icon={faYoutube as IconProp}
+              />
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
