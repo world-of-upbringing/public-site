dfda456545c80ddfbbb881109f95d6e0d597b0de
Mohan Karthik <nemesis.nitt@gmail.com>
2022-11-23 17:01:27 +0530

    Draft

--- netlify/functions/record-payment.ts
+++ netlify/functions/record-payment.ts
@@ -1,8 +1,11 @@
 import { Handler } from "@netlify/functions";
 import * as crypto from "crypto";
 import { google } from "googleapis";
-
+import axios from 'axios';
 import { MongoClient } from "mongodb";
+import { arrayBuffer } from "stream/consumers";
+const FormData = require('form-data');
+
 const mongoClient = new MongoClient(process.env.MONGODB_URI ?? "");
 const clientPromise = mongoClient.connect();
 
@@ -15,16 +18,19 @@ const handler: Handler = async (event, context) => {
   console.log(`Received record-payment request with ${params}`);
 
   // Check if the mac is valid
-  if (!_isTransactionValid(params)) {
-    return {
-      statusCode: 400,
-      body: "Invalid transaction",
-    };
-  }
+  // if (!_isTransactionValid(params)) {
+  //   return {
+  //     statusCode: 400,
+  //     body: "Invalid transaction",
+  //   };
+  // }
+
+  // Get further transaction details from Instamojo
+  const details = await _getPaymentDetails(params.get('payment_id') ?? '')
 
   // Save the data to DB
   const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
-  const collection = database.collection(process.env.MONGODB_COLLECTION ?? "");
+  const collection = database.collection(process.env.MONGODB_COLLECTION_PAYMENTS ?? "");
   collection.insertOne(params);
 
   // Update the date to Google sheets
@@ -33,6 +39,39 @@ const handler: Handler = async (event, context) => {
   // Return success
   return { statusCode: 200 };
 };
+
+const util = require('util')
+
+const _getPaymentDetails = async (id: string): Promise<{}> => {
+  var token_data = new FormData();
+  token_data.append('grant_type', 'client_credentials');
+  token_data.append('client_id', '7CusYRe5DAHjLCBkpKEi1V8mo2T38cCNp4Thh46k');
+  token_data.append('client_secret', '0GrsSCDEraFpqcCZ2iRqzaBFyjkktAoIN3jNbWOR9tmdYgghNtkUQzwOUqkdfmhAki7j8FBPc7jFi4FLAikgp65Q3k6re7YpbEPdiuHNXcRsqaib1IrARDUlFd3hNxbA');
+  console.log('Initiating token');
+  const token_resp = await axios.post(`https://api.instamojo.com/oauth2/token/`, token_data, {
+    headers: {
+        ...token_data.getHeaders()
+    }
+  });
+  if (token_resp.status >= 300) {
+    throw new Error(`Instamojo authentication failed with message: ${token_resp}`)
+  }
+  console.log(`Got token with status ${token_resp.statusText}`);
+
+  let payment_details_resp;
+  try {
+  payment_details_resp = await axios.get(`https://api.instamojo.com/v2/payments/${id}`, {headers: {authorization: `Bearer i-WuQNKyHsRpD6C93TbASsCY6CPU1WfiN2R_DjN772o.u9Jodcc6pGaOtp2nsenWIqKJYLQ_GxQqeyIzHYlAxUE`}});
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+  if (payment_details_resp?.status >= 300) {
+    throw new Error(`Can't get payment details from Instamojo ${payment_details_resp}`)
+  }
+  console.log(payment_details_resp.data);
+
+  return payment_details_resp?.data;
+}
 
 // Function to update the Google sheets with the payment details
 const _updateSheet = async (params: URLSearchParams): Promise<void> => {
